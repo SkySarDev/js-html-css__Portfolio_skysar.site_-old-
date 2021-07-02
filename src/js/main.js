d7fd3b257ee43e81656b2de2c13d7436a4aec21f
SkySar <skysar@ya.ru>
2021-07-02 19:32:03 +0300

    Feat: add js modules folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
+import menu from '../html/_menu.html';
+import header from '../html/_header.html';
+import main from '../html/_main.html';
+import footer from '../html/_footer.html';
+
 import '../scss/main.scss';
 import '../index.html';
+
+document.querySelector('.menu').innerHTML = menu;
+document.querySelector('.header').innerHTML = header;
+document.querySelector('.main').innerHTML = main;
+document.querySelector('.footer').innerHTML = footer;
