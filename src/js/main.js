c76363338e65480d278de5b24570cebb8fba417f
SkySar <skysar@ya.ru>
2021-07-26 22:53:20 +0300

    stylization of the menu, about, projects for XS breakpoint

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,13 @@ import github from '../img/icons/github.svg';
 import instagram from '../img/icons/instagram.svg';
 import telegram from '../img/icons/telegram.svg';
 import vk from '../img/icons/vk.svg';
-import rus from '../img/icons/rus.svg';
-import eng from '../img/icons/eng.svg';
+import langRu from '../img/icons/lang-ru.svg';
+import langEn from '../img/icons/lang-en.svg';
 import arrowSlider from '../img/icons/arrow-slider.svg';
 import skillsHTML from '../img/icons/skills-html.svg';
 import skillsCSS from '../img/icons/skills-css.svg';
 import skillsJS from '../img/icons/skills-js.svg';
-import skillsDev from '../img/icons/skills-developer.svg';
+import skillsDev from '../img/icons/skills-linux.svg';
 
 import aboutImg400 from '../img/about-img-400.png';
 import iravasPrev from '../img/projects/preview/irvas.jpg';
