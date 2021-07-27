7ecadd88be0d665bfd8cfcd69bd977147727e0d5
SkySar <skysar@ya.ru>
2021-07-27 15:36:39 +0300

    delete svg-sprite-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,178 +1,180 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const fs = require('fs');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const fs = require("fs");
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
 
-const fileName = ext => (isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`);
+const fileName = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 const optimization = () => {
-    const configObj = {
-        splitChunks: {
-            chunks: 'all',
-        },
-    };
+  const configObj = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
 
-    if (isProd) {
-        configObj.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+  if (isProd) {
+    configObj.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+  }
+
+  return configObj;
+};
+
+const processNestedHtml = (content, loaderContext, resourcePath = "") => {
+  let fileDir =
+    resourcePath === ""
+      ? path.dirname(loaderContext.resourcePath)
+      : path.dirname(resourcePath);
+  const INCLUDE_PATTERN =
+    /\<include src=\"(\.\/)?(.+)\"\/?\>(?:\<\/include\>)?/gi;
+
+  const replaceHtml = (match, pathRule, src) => {
+    if (pathRule === "./") {
+      fileDir = loaderContext.context;
     }
+    const filePath = path.resolve(fileDir, src);
+    loaderContext.dependency(filePath);
+    const html = fs.readFileSync(filePath, "utf8");
+    return processNestedHtml(html, loaderContext, filePath);
+  };
 
-    return configObj;
+  if (!INCLUDE_PATTERN.test(content)) {
+    return content;
+  } else {
+    return content.replace(INCLUDE_PATTERN, replaceHtml);
+  }
 };
 
-const processNestedHtml = (content, loaderContext, resourcePath = '') => {
-    let fileDir = resourcePath === '' ? path.dirname(loaderContext.resourcePath) : path.dirname(resourcePath);
-    const INCLUDE_PATTERN = /\<include src=\"(\.\/)?(.+)\"\/?\>(?:\<\/include\>)?/gi;
-
-    const replaceHtml = (match, pathRule, src) => {
-        if (pathRule === './') {
-            fileDir = loaderContext.context;
-        }
-        const filePath = path.resolve(fileDir, src);
-        loaderContext.dependency(filePath);
-        const html = fs.readFileSync(filePath, 'utf8');
-        return processNestedHtml(html, loaderContext, filePath);
-    };
-
-    if (!INCLUDE_PATTERN.test(content)) {
-        return content;
-    } else {
-        return content.replace(INCLUDE_PATTERN, replaceHtml);
-    }
-};
-
-const processHtmlLoader = (content, loaderContext) => {
-    let newContent = processNestedHtml(content, loaderContext);
-    return newContent;
-};
+const processHtmlLoader = (content, loaderContext) =>
+  processNestedHtml(content, loaderContext);
 
 const plugins = () => {
-    const basePlugins = [
-        new MiniCssExtractPlugin({
-            filename: `./css/${fileName('css')}`,
-        }),
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, './src/assets'),
-                    to: path.resolve(__dirname, './dist/assets'),
-                },
-            ],
-        }),
-        new CleanWebpackPlugin(),
-    ];
+  const basePlugins = [
+    new MiniCssExtractPlugin({
+      filename: `./css/${fileName("css")}`,
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "./dist/assets"),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ];
 
-    if (isProd) {
-        basePlugins.push(
-            new ImageMinimizerPlugin({
-                minimizerOptions: {
-                    plugins: [
-                        ['gifsicle', { interlaced: true }],
-                        ['jpegtran', { progressive: true }],
-                        ['optipng', { optimizationLevel: 5 }],
-                    ],
-                },
-            })
-        );
-    }
+  if (isProd) {
+    basePlugins.push(
+      new ImageMinimizerPlugin({
+        minimizerOptions: {
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["jpegtran", { progressive: true }],
+            ["optipng", { optimizationLevel: 5 }],
+          ],
+        },
+      })
+    );
+  }
 
-    return basePlugins;
+  return basePlugins;
 };
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    target: isDev ? 'web' : 'browserslist',
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/js/main.js'),
-    output: {
-        clean: true,
-        path: path.resolve(__dirname, './dist'),
-        filename: `./js/${fileName('js')}`,
-        assetModuleFilename: './img/[name][ext][query]',
-    },
-    optimization: optimization(),
-    plugins: plugins(),
-    devtool: isProd ? false : 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
+  context: path.resolve(__dirname, "src"),
+  target: isDev ? "web" : "browserslist",
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/js/main.js"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: `./js/${fileName("js")}`,
+    assetModuleFilename: "./img/[name][ext][query]",
+  },
+  optimization: optimization(),
+  plugins: plugins(),
+  devtool: isProd ? false : "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + "/";
+              },
             },
-            {
-                test: /\.(s[ac]ss|css)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: (resourcePath, context) => {
-                                return path.relative(path.dirname(resourcePath), context) + '/';
-                            },
-                        },
-                    },
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-                options: {
-                    sources: false,
-                    minimize: false,
-                    esModule: false,
-                    preprocessor: processHtmlLoader,
-                },
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: './fonts/[name][ext]',
-                },
-            },
-            {
-                test: /\.(?:ico)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: '[name][ext]',
-                },
-            },
-            {
-                test: /\.svg$/,
-                use: ['svg-sprite-loader', 'svgo-loader'],
-            },
-            {
-                test: /\.(?:gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: '[path][name][ext]',
-                },
-            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
-    },
-    devServer: {
-        port: 7777,
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, './dist'),
-        hot: true,
-        compress: true,
-        open: false,
-    },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          sources: false,
+          minimize: false,
+          esModule: false,
+          preprocessor: processHtmlLoader,
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.(?:ico)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
+      },
+      // {
+      //     test: /\.svg$/,
+      //     use: ['svg-sprite-loader', 'svgo-loader'],
+      // },
+      {
+        test: /\.(?:gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "[path][name][ext]",
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 7777,
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+    compress: true,
+    open: false,
+  },
 };
