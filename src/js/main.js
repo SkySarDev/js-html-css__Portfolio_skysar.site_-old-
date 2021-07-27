7da73480010744aa2a8addbaaafa2c414b8ec6f2
SkySar <skysar@ya.ru>
2021-07-27 15:35:51 +0300

    Feat: add sprite.svg

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,5 @@
-import '../scss/main.scss';
-if (process.env.NODE_ENV === 'development') import('../index.html');
+import "../scss/main.scss";
+if (process.env.NODE_ENV === "development") import("../index.html");
 
-import github from '../img/icons/github.svg';
-import instagram from '../img/icons/instagram.svg';
-import telegram from '../img/icons/telegram.svg';
-import vk from '../img/icons/vk.svg';
-import langRu from '../img/icons/lang-ru.svg';
-import langEn from '../img/icons/lang-en.svg';
-import arrowSlider from '../img/icons/arrow-slider.svg';
-import skillsHTML from '../img/icons/skills-html.svg';
-import skillsCSS from '../img/icons/skills-css.svg';
-import skillsJS from '../img/icons/skills-js.svg';
-import skillsDev from '../img/icons/skills-linux.svg';
-
-import aboutImg400 from '../img/about-img-400.png';
-import iravasPrev from '../img/projects/preview/irvas.jpg';
+import aboutImg400 from "../img/about-img-400.png";
+import iravasPrev from "../img/projects/preview/irvas.jpg";
