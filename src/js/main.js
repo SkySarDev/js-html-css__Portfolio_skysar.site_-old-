5a0a6161fa11337d10d018e64ba2fbda785ecd0a
SkySarDev <skysar@ya.ru>
2021-08-10 13:53:56 +0300

    Feat: add project 3d-glo

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,8 @@ import "../scss/main.scss";
 
 import "../img/projects/preview/irvas.jpg";
 import "../img/projects/preview/be_.jpg";
-import "../img/projects/preview/relax-life.png";
+import "../img/projects/preview/3d-glo.jpg";
+import "../img/projects/preview/relax-live.jpg";
 import "../img/projects/full/be-320w.png";
 import "../img/projects/full/be-470w.png";
 import "../img/projects/full/be-700w.png";
@@ -15,6 +16,9 @@ import "../img/projects/full/irvas-700w.png";
 import "../img/projects/full/relax-live-320w.png";
 import "../img/projects/full/relax-live-470w.png";
 import "../img/projects/full/relax-live-700w.png";
+import "../img/projects/full/3d-glo-320w.png";
+import "../img/projects/full/3d-glo-470w.png";
+import "../img/projects/full/3d-glo-700w.png";
 
 import handlers from "./modules/handlers";
 
